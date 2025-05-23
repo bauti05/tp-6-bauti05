9a2125a0e67ecfcca70510408b2d5878789b4f2c
bauti05 <bperez@mail.austral.edu.ar>
2025-05-22 22:23:25 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,49 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    lista= []
+
+    for i in range(len(list_to_remove_elements)):
+
+        if i != 0 and i != 4 and i!= 5:
+            lista.append(list_to_remove_elements[i])
+
+    return lista
+
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.append("Yellow")
+
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+
+    if len(list_to_check) == 0:
+        return "Lista vacía" 
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    if len(list_to_compare1) > 2 and len(list_to_compare2) > 2: 
+        if list_to_compare1[2] != list_to_compare2[2]:
+            return False
+
+
+        elif list_to_compare1[2] == list_to_compare2[2]:
+            return True
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    
+    lista = []
+
+    if len(list_of_lists_to_modify) >= 3:
+
+        lista.append(list_of_lists_to_modify[0][:2])
+        lista.append(list_of_lists_to_modify[1][1:4])
+        lista.append(list_of_lists_to_modify[2][-2:])
+
+    return lista
+
